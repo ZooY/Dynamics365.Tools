e75c1ec2220575ee0914907e89a2e609ba42264f
Roman Kopaev <kopaevra@gpbl.ru>
2019-12-02 15:41:40 +0300

    UPD Обновлена базовая библиотека работы в Workflow.

--- Source/Tools/DaData/DaData/Properties/AssemblyInfo.cs
+++ Source/Tools/DaData/DaData/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DaData")]
+[assembly: AssemblyTitle("DaData Tools")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DaData")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("Programming Zone")]
+[assembly: AssemblyProduct("CRM Tools")]
+[assembly: AssemblyCopyright("Copyright © 2017-2019 Programming Zone")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("8.0.2.0")]
+[assembly: AssemblyFileVersion("8.0.2.0")]
