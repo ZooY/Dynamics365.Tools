ff639c45b99339359594ff1e7cc9142083740e01
Roman Kopaev <roman@kopaev.ru>
2021-03-12 00:06:39 +0300

    no message

---
+++ Source/Tools/Math/Math/Workflow/Division.cs
@@ -0,0 +1,43 @@
+﻿using System.Activities;
+using Microsoft.Xrm.Sdk.Workflow;
+using PZone.Xrm.Workflow;
+
+
+namespace PZone.MathTools.Workflow
+{
+    /// <summary>
+    /// Получение частного двух чисел.
+    /// </summary>
+    public class Division : WorkflowBase
+    {
+        /// <summary>
+        /// Делимое.
+        /// </summary>
+        [Input("Dividend")]
+        [RequiredArgument]
+        public InArgument<decimal> DividendValue { get; set; }
+
+
+        /// <summary>
+        /// Делитель.
+        /// </summary>
+        [Input("Divisor")]
+        [RequiredArgument]
+        public InArgument<decimal> DivisorValue { get; set; }
+
+
+        /// <summary>
+        /// Частное.
+        /// </summary>
+        [Output("Quotient ")]
+        public OutArgument<decimal> QuotientValue { get; set; }
+
+
+        protected override void Execute(Context context)
+        {
+            var dividendValue = DividendValue.Get(context);
+            var divisorValue = DivisorValue.Get(context);
+            QuotientValue.Set(context, dividendValue / divisorValue);
+        }
+    }
+}
---
+++ Source/Tools/Math/Math/Workflow/Multiplication.cs
@@ -0,0 +1,43 @@
+﻿using System.Activities;
+using Microsoft.Xrm.Sdk.Workflow;
+using PZone.Xrm.Workflow;
+
+
+namespace PZone.MathTools.Workflow
+{
+    /// <summary>
+    /// Получение произведения двух чисел.
+    /// </summary>
+    public class Multiplication : WorkflowBase
+    {
+        /// <summary>
+        /// Множимое.
+        /// </summary>
+        [Input("Multiplicand")]
+        [RequiredArgument]
+        public InArgument<decimal> MultiplicandValue { get; set; }
+
+
+        /// <summary>
+        /// Множитель.
+        /// </summary>
+        [Input("Multiplier")]
+        [RequiredArgument]
+        public InArgument<decimal> MultiplierValue { get; set; }
+
+
+        /// <summary>
+        /// Произведение.
+        /// </summary>
+        [Output("Product")]
+        public OutArgument<decimal> ProductValue { get; set; }
+
+
+        protected override void Execute(Context context)
+        {
+            var multiplicandValue = MultiplicandValue.Get(context);
+            var multiplierValue = MultiplierValue.Get(context);
+            ProductValue.Set(context, multiplicandValue * multiplierValue);
+        }
+    }
+}
